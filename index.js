bc33af1e697b041ca0ca08c26427222153f52772
“hunganh2004” <hunganh2004hy@gmail.com>
2025-09-29 22:53:35 +0700

    Fix redirect

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { nanoid } from "nanoid";
 const app = express();
 app.use(express.json());
 
-// Dùng object tạm thay DB (sau này có thể thay MySQL/MongoDB)
 const db = {};
 
 // API rút gọn link
@@ -14,21 +13,22 @@ app.post("/api/shorten", (req, res) => {
 
     const code = nanoid(6);
     db[code] = url;
-    res.json({ shortUrl: `https://your-vercel-project.vercel.app/${code}` });
-});
 
-// Redirect khi truy cập link ngắn
-app.get("/:code", (req, res) => {
-    const { url } = req.body;
-    if (!url) return res.status(400).json({ error: "Thiếu URL" });
-
-    const code = nanoid(6);
-    db[code] = url;
-
+    // 🔹 Lấy domain thật từ request thay vì hardcode
     const host = req.headers.host;
-    const protocol = req.headers["x-forwarded-proto"] || "http";
+    const protocol = req.headers["x-forwarded-proto"] || "https";
 
     res.json({ shortUrl: `${protocol}://${host}/${code}` });
 });
 
-export default app
+// Redirect khi truy cập link ngắn
+app.get("/:code", (req, res) => {
+    const url = db[req.params.code];
+    if (url) {
+        res.redirect(url);
+    } else {
+        res.status(404).send("Link không tồn tại!");
+    }
+});
+
+export default app;
